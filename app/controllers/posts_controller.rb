d212cb4a89327d6b8897ac966ece57bfcf1620c4
Cassio Pereira <cassio.pereira@gmail.com>
2016-05-01 15:24:12 -0700

    Actions and strong parameters implementations for Posts and Comments.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+
+  def index
+    render json: Post.all
+  end
+
+  def create
+    render json: Post.create(post_params)
+  end
+
+  def show
+    render json: Post.find(params[:id])
+  end
+
+  def upvote
+    post = Post.find(params[:id])
+    post.increment!(:upvotes)
+
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:link, :title)
+  end
 end
