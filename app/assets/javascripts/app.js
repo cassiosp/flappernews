0746a4c32a14d63cd139bcd052d1470927c5b2b8
Cassio Pereira <cassio.pereira@gmail.com>
2016-05-01 15:29:15 -0700

    Post loading from server and related resolve implementation to ensure data load on home state load.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,7 +8,15 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: 'home/_home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: [
+          'posts', 
+          function(posts){
+            return posts.getAll();
+          }
+        ]
+      }
     })
     .state('posts', {
       url: '/posts/{id}',
